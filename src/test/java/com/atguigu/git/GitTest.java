b0d9abfe7cec63d5497d60cf644fcb378b3e0f0a
旅思 <113096695+g4k2@users.noreply.github.com>
2023-11-21 23:41:55 +0800

    Update GitTest.java

--- src/test/java/com/atguigu/git/GitTest.java
+++ src/test/java/com/atguigu/git/GitTest.java
@@ -1,7 +1,9 @@
+
 package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("this is in the github xiugai");
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
