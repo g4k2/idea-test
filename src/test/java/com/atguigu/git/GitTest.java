8aa8fbd20cc4436686dc4601fc56e45f4cb30a57
lvsi <3272734918@qq.com>
2023-11-21 19:58:27 +0800

    push commit

--- src/test/java/com/atguigu/git/GitTest.java
+++ src/test/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("this is hot.fix");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
